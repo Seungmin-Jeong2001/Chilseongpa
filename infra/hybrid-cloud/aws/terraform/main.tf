d3e727daf7ecac557b3ad9b1f76c5bb348cfbc3b
hjh6709 <jh6780h@naver.com>
2026-03-19 16:36:35 +0900

    s3 bucket 주석처리

--- infra/hybrid-cloud/aws/terraform/main.tf
+++ infra/hybrid-cloud/aws/terraform/main.tf
@@ -13,11 +13,11 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "hybrid-project-terraform-state"
-    key            = "aws-standby/terraform.tfstate"
-    region         = "ap-northeast-2"
-    dynamodb_table = "hybrid-project-terraform-lock"
-    encrypt        = true
-  }
+  # backend "s3" {
+  #  bucket         = "hybrid-project-terraform-state"
+  #  key            = "aws-standby/terraform.tfstate"
+  #  region         = "ap-northeast-2"
+  #  dynamodb_table = "hybrid-project-terraform-lock"
+  #  encrypt        = true
+  # }
 }
