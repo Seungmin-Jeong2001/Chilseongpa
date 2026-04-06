56cd042c0968f0646806e81bbdd5faf95d4010d5
Jeong <mis084110@gmail.com>
2026-04-06 15:12:27 +0900

    변수 실수

--- infra/terraform/modules/gcp/variables.tf
+++ infra/terraform/modules/gcp/variables.tf
@@ -69,9 +69,3 @@ variable "monitoring_tunnel_token" {
   type        = string
   sensitive   = true
 }
-
-variable "cf_discord_webhook_url" {
-  description = "Discord Webhook URL for Cloudflare Alerts"
-  type        = string
-  sensitive   = true # 보안을 위해 민감한 정보로 표시
-}
